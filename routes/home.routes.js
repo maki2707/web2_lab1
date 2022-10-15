069188f4cd7f1720c35ad3f966ab2cd8e22e1255
maki2707 <filip.marcec@fer.hr>
2022-10-16 00:19:49 +0200

    dodan prikaz tablice

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -2,13 +2,12 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db');
 
-router.get('/', async function(req, res) {
-    var list = (await db.query('SELECT * FROM test1')).rows
-    console.log(list)
+router.get('/', async function(req, res) {    
+    var tablica = (await db.query('SELECT * FROM tablica ORDER BY bodovi DESC, "golRazlika" DESC')).rows
     res.render('home', {
         title: 'Home',
-        linkActive: 'home',
-        list: list
+        linkActive: 'home',       
+        tablica: tablica
     });
 });
 
